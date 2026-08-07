822b97e0c0fdbc5cbb0d9031dd2b85325846bc1b
Anil Hitang <anilhitang@MacBook-Pro.local>
2026-08-07 10:21:36 -0300

    Initital project setup

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col bg-white">
+      <main className="flex-1">
+        {/* Here Section */}
+        <section className="container mx-auto px-4 py-32">
+          <div>
+            <h1 className="text-black">
+              A better way to track your job application.
+            </h1>
+            <p className="text-gray-700">
+              Capture, organise, and manage your job search in one place.
+            </p>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
