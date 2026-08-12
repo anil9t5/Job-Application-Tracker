6eed2d4f8ea4d5e840feffa0235312bd01674916
Anil Hitang <anilhitang@MacBook-Pro.local>
2026-08-12 13:08:01 -0300

    Updated navigation bar to show authenicated logic and profile icon dropdown in progress

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,19 @@
+"use client";
 import { Briefcase } from "lucide-react";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import SignOutButton from "./sign-out-btn";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import { useSession } from "@/lib/auth/auth-client";
 
 export default function Navbar() {
+  const { data: session } = useSession();
   return (
     <nav className="borderr-b border-gray-200 bg-white">
       <div className="container mx-auto flex h-16 items-center px-4 justify-between">
@@ -14,16 +25,54 @@ export default function Navbar() {
           Tracker
         </Link>
         <div className="flex items-center gap-4">
-          <Link href="/sign-in">
-            <Button variant="ghost" className="text-gray-700 hover:text-black">
-              Log in
-            </Button>
-          </Link>
-          <Link href="/sign-up">
-            <Button className="bg-primary hover:bg-primary/90">
-              Start for free
-            </Button>
-          </Link>
+          {session?.user ? (
+            <>
+              <Link href="/dashboard">
+                <Button
+                  variant="ghost"
+                  className="text-gray-700 hover:text-black"
+                >
+                  Dashboard
+                </Button>
+              </Link>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Button variant="ghost">
+                    <Avatar>
+                      <AvatarFallback className="bg-primary text-white">
+                        {session.user.name[0].toUpperCase()}
+                      </AvatarFallback>
+                    </Avatar>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>
+                    <div>
+                      <p>{session.user.name}</p>
+                      <p>{session.user.email}</p>
+                    </div>
+                  </DropdownMenuLabel>
+                  <SignOutButton />
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </>
+          ) : (
+            <>
+              <Link href="/sign-in">
+                <Button
+                  variant="ghost"
+                  className="text-gray-700 hover:text-black"
+                >
+                  Log in
+                </Button>
+              </Link>
+              <Link href="/sign-up">
+                <Button className="bg-primary hover:bg-primary/90">
+                  Start for free
+                </Button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </nav>
