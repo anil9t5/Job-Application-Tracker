777c7020f086b37f4af15778bc0f9f9a6714cce8
Anil Hitang <anilhitang@MacBook-Pro.local>
2026-08-20 12:03:03 -0300

    Successfully fetched Job Application Board for the user on him Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,6 @@
 import { getSession } from "@/lib/auth/auth";
+import connectDB from "@/lib/db";
+import { Board } from "@/lib/models";
 import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
@@ -7,5 +9,15 @@ export default async function Dashboard() {
   if (!session?.user) {
     redirect("/sign-in");
   }
+
+  await connectDB();
+
+  const board = await Board.findOne({
+    userId: session.user.id,
+    name: "Job Hunt",
+  });
+
+  console.log(board);
+
   return <div>Dashboard page</div>;
 }
