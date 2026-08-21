c4ede35ee6b654dc41d19d07cee6f940343bfe60
Anil Hitang <anilhitang@MacBook-Pro.local>
2026-08-21 16:54:38 -0300

    Added board card layout with Card Header in the Dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,23 +1,48 @@
+import KanbanBoard from "@/components/kanban-board";
 import { getSession } from "@/lib/auth/auth";
 import connectDB from "@/lib/db";
 import { Board } from "@/lib/models";
 import { redirect } from "next/navigation";
 
+async function getBoard(userId: string) {
+  await connectDB();
+
+  const boardDoc = await Board.findOne({
+    userId: userId,
+    name: "Job Hunt",
+  }).populate({
+    path: "columns",
+  });
+
+  if (!boardDoc) return null;
+
+  const board = JSON.parse(JSON.stringify(boardDoc));
+
+  return board;
+}
+
 export default async function Dashboard() {
   const session = await getSession();
+  const board = await getBoard(session?.user.id ?? "");
 
   if (!session?.user) {
     redirect("/sign-in");
   }
 
-  await connectDB();
-
-  const board = await Board.findOne({
-    userId: session.user.id,
-    name: "Job Hunt",
-  });
-
-  console.log(board);
-
-  return <div>Dashboard page</div>;
+  return (
+    <div>
+      <div className="min-h-screen bg-white">
+        <div className="container mx-auto p-6">
+          <div className="mb-6">
+            <h1 className="text-3xl font-bold text-black">Dashboard title</h1>
+            <p className="text-gray-600">Track your job applications</p>
+          </div>
+          <KanbanBoard
+            board={JSON.parse(JSON.stringify(board))}
+            userId={session.user.id}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
